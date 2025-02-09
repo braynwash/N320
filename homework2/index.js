5230fd50b86c0076a3147a5bfa49ce59601a9ea3
jeremy !! <f.tzgeraldz@gmail.com>
2025-02-09 17:41:48 -0500

    first commit

---
+++ homework2/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
+
+const app = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Your project is ready to be created!");
+
+app.question("What is the name of your project? ", (projectName) => {
+  const filePath = path.resolve("output", `${projectName}.txt`);
+
+  if (!fs.existsSync("output")) {
+    fs.mkdirSync("output");
+  }
+
+  fs.writeFileSync(filePath, projectName);
+
+  console.log(`Project created: ${filePath}`);
+  app.close();
+});
